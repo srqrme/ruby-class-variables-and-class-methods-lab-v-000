8896f92d9a536feffcbf1cdad0268c2d628ee1c4
Sharmaine Requerme <srqrme@gmail.com>
2018-08-06 15:58:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,17 @@
 class Song
+
+  @@count = 0
+
   attr_accessor :name, :artist, :genre
 
-  def initialize
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
 end
-  

fb49c00b435f5bfc8f559ab977ec66cf28c3543e
Sharmaine Requerme <srqrme@gmail.com>
2018-08-06 16:46:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,6 +24,10 @@ class Song
   end
 
   def self.artist_count
+    artist_count = {}
+
+    @@artists.each do |artist|
+      artist_count[artist] ? artist_count[artist] += 1 : artist_count[artist] = 1
   end
 
   def self.genres
